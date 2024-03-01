6c51cf387485ffb5b1061db9d6f5cb47d0164158
jahmed <jahmed@ethz.ch>
2024-03-01 11:25:58 +0100

    main.py and Readme.md Updated

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # DoAQuiz
+
+## Overview
+
+An alli wo Nerve verlüred bim DoAQuiz vom Modul 145 ich han Es Script gschriebe für die Frage Type:
+
+- Network address of 175.83.224.0/20?
+- Divide 175.83.224.0/20 by 6. Third Resulting Subnet
+- Broadcast address of 175.83.224.0/20
+
+Für die andere Ufgabe bin ich mir sicher, dass ihr gnueg Fähig sind vonere Subnetztabelle abzulese: https://www.connecteddots.online/resources/blog/subnet-masks-table
+## Usage
+
+1. **Divide Subnet:**
+    - Choose option 1 when prompted.
+    - Enter the IP address and CIDR (e.g., `175.83.224.0/20`).
+    - Input the division and part when prompted.
+
+2. **Calculate Broadcast Address:**
+    - Choose option 2 when prompted.
+    - Enter the IP address and CIDR (e.g., `175.83.224.0/20`).
+
+3. **Calculate Network Address:**
+    - Choose option 3 when prompted.
+    - Enter the IP address and CIDR (e.g., `175.83.224.0/20`).
+
+4. **Exit:**
+    - Choose option 3 when you want to exit the program.
+
+## Requirements
+
+- Python 3.x
+
+## How to Run
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/JosephAdama/DoAQuiz.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd DoAQuiz
+    ```
+
+3. Run the script:
+
+    ```bash
+    python main.py
+    ```
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ main.py
@@ -0,0 +1,114 @@
+import math
+
+
+def is_valid_ip(ip):
+    parts = ip.split('/')
+    if len(parts) != 2:
+        return False
+    ip_parts = parts[0].split('.')
+    if is_valid_cidr(parts[1]) and len(ip_parts) == 4 and all(
+            part.isdigit() and 0 <= int(part) <= 255 for part in ip_parts):
+        return True
+    return False
+
+
+def is_valid_cidr(cidr):
+    try:
+        cidr_value = int(cidr)
+        return 0 <= cidr_value <= 32
+    except ValueError:
+        return False
+
+
+def Divide_Subnet(ip, division, part):
+    ip_parts = ip.split('/')
+    ip_address = ip_parts[0]
+    cidr = int(ip_parts[1])
+
+    res_division = int(2 ** (division - 1).bit_length())
+    host_b = int(32 - cidr - math.log2(res_division))
+    new_cidr = int(32 - host_b)
+
+    ip_b = '.'.join([bin(int(x) + 256)[3:] for x in ip_address.split('.')])
+
+    b_string = ip_b.replace(".", "")
+    host_b_string = b_string[new_cidr:]
+    network_b_string = b_string[:new_cidr]
+    result_network_b_string = format(int(network_b_string, 2) + part - 1, 'b').zfill(len(network_b_string))
+    result_b_string = result_network_b_string + host_b_string
+
+    result_ip = ".".join(map(str, [int(segment, 2) for segment in
+                                   [result_b_string[i:i + 8] for i in range(0, len(result_b_string), 8)]]))
+
+    return result_ip + "/" + str(new_cidr)
+
+
+def calculate_broadcast_address(ip):
+    ip_parts = ip.split('/')
+    ip_address = ip_parts[0]
+    host_b = 32 - int(ip_parts[1])
+
+    ip_b = '.'.join([bin(int(x) + 256)[3:] for x in ip_address.split('.')])
+    result_b_string = ip_b.replace(".", "")[:-host_b] + "1" * host_b
+
+    result_ip = ".".join(map(str, [int(segment, 2) for segment in
+                                   [result_b_string[i:i + 8] for i in range(0, len(result_b_string), 8)]]))
+    return result_ip
+
+
+def calculate_network_address(ip):
+    ip_parts = ip.split('/')
+    ip_address = ip_parts[0]
+    host_b = 32 - int(ip_parts[1])
+
+    ip_b = '.'.join([bin(int(x) + 256)[3:] for x in ip_address.split('.')])
+    result_b_string = ip_b.replace(".", "")[:-host_b] + "0" * host_b
+
+    result_ip = ".".join(map(str, [int(segment, 2) for segment in
+                                   [result_b_string[i:i + 8] for i in range(0, len(result_b_string), 8)]]))
+    return result_ip
+
+
+if __name__ == '__main__':
+    while True:
+        print("Choose an option:")
+        print("1. Divide Subnet")
+        print("2. Calculate Broadcast Address")
+        print("3. Calculate Network Address")
+        print("4. Exit")
+
+        choice = input("Enter your choice (1, 2, 3 or 4): ")
+
+        if choice == '1':
+            ip = input("Enter IP address and CIDR (e.g., 175.83.224.0/20): ")
+            division = input("Enter division: ")
+            part = input("Enter part: ")
+
+            if is_valid_ip(ip) and division.isdigit() and part.isdigit():
+                print(f"The Subnet is {Divide_Subnet(ip, int(division), int(part))}\n\n")
+            else:
+                print("Invalid input. Please enter a valid IP address, CIDR, and numeric values for division and "
+                      "part.\n")
+
+        elif choice == '2':
+            ip = input("Enter IP address and CIDR (e.g., 175.83.224.0/20): ")
+
+            if is_valid_ip(ip):
+                print(f"The broadcast address is: {calculate_broadcast_address(ip)}\n\n")
+            else:
+                print("Invalid input. Please enter a valid IP address.\n")
+
+        elif choice == '3':
+            ip = input("Enter IP address and CIDR (e.g., 175.83.224.0/20): ")
+
+            if is_valid_ip(ip):
+                print(f"The Network address is: {calculate_network_address(ip)}\n\n")
+            else:
+                print("Invalid input. Please enter a valid IP address.\n")
+
+        elif choice == '4':
+            print("Exiting the program.")
+            break
+
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.\n")
